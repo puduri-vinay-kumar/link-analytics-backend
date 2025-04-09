f0422af69a253b33e7d038394da0c6122528297d
puduri-vinay-kumar <vinaykumarpuduri10@gmail.com>
2025-04-09 22:29:06 +0530

    added

--- url-shortener-backend/middlewares/authMiddleware.js
+++ url-shortener-backend/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User'); 
 
 const authenticate = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
