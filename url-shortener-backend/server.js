3cb17e37ea3b1d72c18932eccb743b5fd063f5fe
puduri-vinay-kumar <vinaykumarpuduri10@gmail.com>
2025-04-09 22:49:38 +0530

    added

--- url-shortener-backend/server.js
+++ url-shortener-backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/links', require('./routes/linkRoutes'));
-
+app.use('/api/links', require('./routes/linkRoutes'));ss  
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(process.env.PORT, () => {
